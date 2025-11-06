f5069ee0221a9106dd43b79ad6d1cbb729ffdba8
tygrysek90 <risa.szlachta@gmail.com>
2025-11-06 05:53:10 +0100

    Write crash-causing code

--- src/mainActions.ts
+++ src/mainActions.ts
@@ -2,3 +2,19 @@
  * GUI functions
  */
 
+import { Colour } from "openrct2-flexui" // enum Colour {Black = 0, Grey = 1, White = 2,...
+import { debug, error } from "./logger"  // these are just fancy console.log() wrappers
+
+export function onButtonClick() {
+    let range = ui.imageManager.allocate(1)
+    debug(JSON.stringify(range))
+    if (range != null) {
+        ui.imageManager.draw(range.start, {width: 50, height: 50} satisfies ScreenSize, (g:GraphicsContext) => {
+            g.colour = Colour.Black   // 0
+            g.line(0,0,49,49)
+        })
+    }
+    else {
+        error("can not get image alloc range")
+    }
+}
--- src/mainWin.ts
+++ src/mainWin.ts
@@ -3,17 +3,19 @@
  * GUI definition
  */
 
-import { Colour, label, window } from "openrct2-flexui";
+import { button, Colour, window } from "openrct2-flexui";
 import { pluginName } from "./environment";
+import { onButtonClick } from "./mainActions";
 
 export const mainWindow = window({
     title: pluginName,
     width: 200,
-    height: 200,
+    height: 40,
     colours: [Colour["Grey"], Colour["Grey"]],
     content: [
-        label({
-            text: "Hello world!"
+        button({
+            text: "Run code and cause game to crash",
+            onClick: () => onButtonClick(),
         }),
     ]
 })
